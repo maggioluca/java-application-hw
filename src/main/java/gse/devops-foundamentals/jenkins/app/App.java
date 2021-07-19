fcff1678dffab7e4acfe5c3d23a294dd3371888d
Luca Maggio <luca.maggio@keypartner.com>
2021-07-20 00:20:36 +0200

    feat(application-start): print hello world phrase

---
+++ src/main/java/gse/devops-foundamentals/jenkins/app/App.java
@@ -0,0 +1,13 @@
+package gse.devops-foundamentals.jenkins.app;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World from Java Application!" );
+    }
+}
